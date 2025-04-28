064375c9e89acb16b8c268125f1666f90ebdd7ca
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-04-28 18:10:30 -0500

    const and let

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const variableName = "Chris";
+let variableName = "Chris";
+console.log(variableName); // Chris
 
 variableName = 25;
+console.log(variableName); // 25
 
 variableName = true;
+console.log(variableName); // true
 
 variableName = [654, "hey hey hey", {}];
+console.log(variableName); // [654, "hey hey hey", {}]
 
-let someVariableName = "Jimmy";
+const someVariableName = "Jimmy";
+console.log(someVariableName); // Jimmy
+someVariableName = 25; // TypeError: Assignment to constant variable.
